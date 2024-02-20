132e8c9c1cbc566879507920b0f428b61b8feca0
jeong-yooon <102950984+jeong-yooon@users.noreply.github.com>
2024-02-20 20:09:59 +0900

    [FEAT] 주문페이지 제작중 (#11)

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import Navbar from "./Components/Navbar";
 import Home from "./pages/home/Home";
 import Intro from "./Components/Intro";
 import Store from "./Components/Store";
+import Order from "./Components/Order";
+import Success from "./Components/Success";
+import Fail from "./Components/Fail";
 import Restore from "./Components/Restore";
 import Raffle from "./Components/Raffle";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
 import Mypage from "./Components/Mypage";
+import Cart from "./Components/Cart";
 import Footer from "./Components/Footer";
-import Order from "./Components/Order";
+
 
 const App = () => {
   return (
@@ -22,11 +26,15 @@ const App = () => {
             <Route path="/intro" element={<Intro />} />
             <Route path="/store" element={<Store />} />
             <Route path="/order" element={<Order />} />
+            <Route path="/success" element={<Success />} />
+            <Route path="/success" element={<Fail />} />
             <Route path="/restore" element={<Restore />} />
             <Route path="/raffle" element={<Raffle />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/mypage" element={<Mypage />} />
+            <Route path="/mypage" element={<Mypage />} >
+              <Route path="cart" element={<Cart />} />
+            </Route>
           </Routes>
         <Footer/>
       </div>
