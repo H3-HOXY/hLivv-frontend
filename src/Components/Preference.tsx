a17024f1e700cd9865f33b04d2d7974be0951a91
jeong-yooon <chjy1819@gmail.com>
2024-02-27 12:10:45 +0900

    [CHORE] 결제 페이지 구현 완료

--- src/Components/Preference.tsx
+++ src/Components/Preference.tsx
@@ -1,17 +1,10 @@
 import "../Components_scss/Preference.scss"
-import { useImage } from "../pages/common/hooks/useImage";
+import { Outlet } from "react-router-dom";
 
 const Preference = () => {
-  const image = useImage()
   return (
     <div className="Preference">
-      <div className="PreferenceWrapper">
-        <img className="PreferenceLeftImg" src={image("취향메인이미지.png")} title="pic"></img>
-        <div className="PreferenceRight">
-          <img className="PreferenceRightImg" src={image("취향설명이미지.png")} title="pic"></img>
-          <button className="PreferenceBtn">시작하기</button>
-        </div>
-      </div>
+      <Outlet/>
     </div>
   );
 }
