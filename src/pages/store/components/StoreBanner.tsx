91d20d5b014efce4ec662e8c485820e6cd94ade6
hoyeonlee-dev <dlghdus122@gmail.com>
2024-03-06 10:42:14 +0900

    [CHORE] 콜라보 Banner 롤백

--- src/pages/store/components/StoreBanner.tsx
+++ src/pages/store/components/StoreBanner.tsx
@@ -1,5 +1,5 @@
 import Slider from "react-slick";
-import { useImage } from "../../common/hooks/useImage";
+import {useImage} from "../../common/hooks/useImage";
 
 export type StoreSliderProps = {
     sliderItems: StoreSliderItemProps[]
@@ -10,6 +10,14 @@ export type StoreSliderItemProps = {
     title: string
 }
 
+
+export const StoreBannerNoSlide = (props: StoreSliderItemProps) => {
+    return (
+        <div className="py-5 flex justify-center ">
+            <img className="w-full h-auto object-cover w-11/12" src={props.img} alt={props.title}/>
+        </div>
+    )
+}
 export const StoreBanner = (props: StoreSliderProps) => {
     const image = useImage()
     const leftsettings = {
@@ -24,21 +32,21 @@ export const StoreBanner = (props: StoreSliderProps) => {
 
     return (
         <div className="StoreBannerWrapper">
-        {/* 메인 슬라이드 배너 */}
-        <div className="StoreBanner">
-            <div className="StoreSlider">
-                <Slider {...leftsettings}>
-                    {props.sliderItems.map((item, idx) => {
-                            return (
-                                <div key={idx} className="StoreSliderItem">
-                                    <img src={image(item.img)} title={item.title} alt={`${item.title}`}/>
-                                </div>
-                            )
-                        }
-                    )}
-                </Slider>
+            {/* 메인 슬라이드 배너 */}
+            <div className="StoreBanner">
+                <div className="StoreSlider">
+                    <Slider {...leftsettings}>
+                        {props.sliderItems.map((item, idx) => {
+                                return (
+                                    <div key={idx} className="StoreSliderItem">
+                                        <img src={image(item.img)} title={item.title} alt={`${item.title}`}/>
+                                    </div>
+                                )
+                            }
+                        )}
+                    </Slider>
+                </div>
             </div>
-        </div>
         </div>
     )
 }
