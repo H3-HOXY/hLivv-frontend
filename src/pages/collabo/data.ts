f28b2114d6c43fdd87f0f0eaf9540f4b20240122
hoyeonlee-dev <dlghdus122@gmail.com>
2024-03-13 13:44:47 +0900

    [CHORE] 콜라보 카테고리 변경

--- src/pages/collabo/data.ts
+++ src/pages/collabo/data.ts
@@ -1,32 +1,22 @@
 export const collaboCategory = [
     {
         id: "1",
-        name: '전체',
-        image: 'collabo/All.png'
+        name: '봄',
+        image: 'collabo/spring.png'
     },
     {
         id: "2",
-        name: '공부방',
-        image: 'collabo/Study.png'
+        name: '여름',
+        image: 'collabo/summer.png'
     },
     {
         id: "3",
-        name: '거실',
-        image: 'collabo/LivingRoom.png'
+        name: '가을',
+        image: 'collabo/autumn.png'
     },
     {
         id: "4",
-        name: '주방',
-        image: 'collabo/Kitchen.png'
-    },
-    {
-        id: "5",
-        name: '자녀방',
-        image: 'collabo/Kids.png'
-    },
-    {
-        id: "6",
-        name: '침실',
-        image: 'collabo/BedRoom.png'
-    },
+        name: '겨울',
+        image: 'collabo/winter.png'
+    }
 ]
